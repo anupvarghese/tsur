de10ff8883ed14650bb9a105fb51c211f853cd11
Anoop Varghese <anupvarghese@gmail.com>
2016-10-16 17:45:13 +1100

    loop it until matches

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,24 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the letter!");
     println!("Pls enter your guess");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read the number");
-    println!("Your guess: {}", guess);
-    let guess: u32 = guess.trim().parse().expect("Pls enter a number");
     let secret_number = rand::thread_rng().gen_range(1, 10);
-    println!("Computer guessed: {}", secret_number);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Smaller bet, lost"),
-        Ordering::Greater => println!("Larger bet, lost"),
-        Ordering::Equal => println!("You win"),
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read the number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, 
+        };
+        println!("Your guess: {}", guess);
+        
+        // println!("Computer guessed: {}", secret_number);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Smaller bet, lost"),
+            Ordering::Greater => println!("Larger bet, lost"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
